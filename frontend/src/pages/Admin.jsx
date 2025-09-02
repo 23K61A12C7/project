c0eec6a8afb758f48b64a0d37406906f88572143
Srinivas Reddy <srinivas@example.com>
2025-09-02 14:46:24 +0530

    deleted

--- frontend/src/pages/Admin.jsx
+++
@@ -1,4 +0,0 @@
-import LogoBar from "../components/LogoBar";
-export default function Admin() {
-  return <h1>Admin Page</h1>;
-}
--- frontend/src/pages/Result.jsx
+++
@@ -1,4 +0,0 @@
-import LogoBar from "../components/LogoBar";
-export default function Result() {
-  return <h1>Result Page</h1>;
-}
